8c46107c3c49f5e3caf308269fc673ca2b45ef33
Loïc Omnes <loic.omnes@free.fr>
2023-01-12 22:44:33 +0100

    remove HTML module

--- run_streamlit.py
+++ run_streamlit.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import base64
-from IPython.core.display import HTML
 
 # Load and prepare data
 df_items = pd.read_csv(r'data/items.csv')
